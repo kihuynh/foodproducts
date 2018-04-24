b68aa94724af2ee8eed8980f2e65da22a86a2284
Kimberly Huynh <kimberly.huynh@gmail.com>
2018-04-23 19:22:50 -0700

    drop db and reseed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,20 +2,20 @@ Product.destroy_all
 Review.destroy_all
 
 50.times do |index|
-  Product.create!(
+  product = Product.create!(
     name: Faker::Food.unique.dish,
     cost: Faker::Commerce.price,
     origin: Faker::Address.country
     )
-end
 
-5.times do |index|
-  Reviews.create!(
-    author: Faker::Book.unique.author,
-    content_body: Faker::Hipster.sentence(50),
-    rating: Faker::Number.between(1, 5),
-    product_id: @product_id
-    )
+  5.times do |index|
+    product.reviews.create!(
+      author: Faker::Book.unique.author,
+      content_body: Faker::Hipster.sentence(50),
+      rating: Faker::Number.between(1, 5),
+      product_id: @product_id
+      )
+  end
 end
 
 p "created #{Product.count} products"
