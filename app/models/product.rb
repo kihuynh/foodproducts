27d455b1695bf4bc931fc06d249ff6eefe4be2f8
Kimberly Huynh <kimberly.huynh@gmail.com>
2018-04-23 21:53:24 -0700

    confirm unique validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ActiveRecord::Base
   has_many :reviews
   validates :name, :presence => true
+  validates :name, :uniqueness => true
   validates :cost, :presence => true
   validates :origin, :presence => true
 
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,7 @@ class Review < ActiveRecord::Base
   belongs_to :product
 
   validates :author, :presence => true
+  validates :author, :uniqueness => true
   validates :content_body, :presence => true
   validates :rating, :presence => true
 
